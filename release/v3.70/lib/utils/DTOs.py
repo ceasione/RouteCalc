421cf53bd177588d4ae30c93f66b113c6781dc8b
Alex Halitsky <ceasione@gmail.com>
2025-01-03 16:08:28 +0200

    Cache emergency mode

---
+++ release/v3.70/lib/utils/DTOs.py
@@ -0,0 +1,121 @@
+
+class LocaleDTO:
+
+    def __init__(self, locale: str):
+
+        if locale == 'ru_UA':
+            self.value = locale
+        elif locale == 'uk_UA':
+            self.value = locale
+        else:
+            raise RuntimeError('Only uk_UA and ru_UA locales are currently supported')
+
+    def is_uk_ua(self):
+        return True if self.value == 'uk_UA' else False
+
+    def is_ru_ua(self):
+        return True if self.value == 'ru_UA' else False
+
+    def to_dict(self):
+        return self.value
+
+    @classmethod
+    def from_dict(cls, dct):
+        return cls(locale=dct['value'])
+
+
+class CalculationDTO:
+    def __init__(self,
+                 place_a_name: str,
+                 place_a_name_long: str,
+                 place_b_name: str,
+                 place_b_name_long: str,
+                 map_link: str,
+                 place_chain: str,
+                 chain_map_link: str,
+                 distance: str,
+                 transport_id: int,
+                 transport_name: str,
+                 transport_capacity: int,
+                 price: str,
+                 price_per_ton: str,
+                 price_per_km: str,
+                 is_price_per_ton: bool,
+                 currency: str,
+                 currency_rate: float,
+                 pfactor_vehicle: str,
+                 pfactor_departure: str,
+                 pfactor_arrival: str,
+                 pfactor_distance: str,
+                 locale: LocaleDTO):
+        self.place_a_name = place_a_name
+        self.place_a_name_long = place_a_name_long
+        self.place_b_name = place_b_name
+        self.place_b_name_long = place_b_name_long
+        self.map_link = map_link
+        self.place_chain = place_chain
+        self.chain_map_link = chain_map_link
+        self.distance = distance
+        self.transport_id = transport_id
+        self.transport_name = transport_name
+        self.transport_capacity = transport_capacity
+        self.price = price
+        self.price_per_ton = price_per_ton
+        self.price_per_km = price_per_km
+        self.is_price_per_ton = is_price_per_ton
+        self.currency = currency
+        self.currency_rate = currency_rate
+        self.pfactor_vehicle = pfactor_vehicle
+        self.pfactor_departure = pfactor_departure
+        self.pfactor_arrival = pfactor_arrival
+        self.pfactor_distance = pfactor_distance
+        self.locale = locale
+
+    def to_dict(self):
+        return {'place_a_name': self.place_a_name,
+                'place_a_name_long': self.place_a_name_long,
+                'place_b_name': self.place_b_name,
+                'place_b_name_long': self.place_b_name_long,
+                'map_link': self.map_link,
+                'place_chain': self.place_chain,
+                'chain_map_link': self.chain_map_link,
+                'distance': self.distance,
+                'transport_id': self.transport_id,
+                'transport_name': self.transport_name,
+                'transport_capacity': self.transport_capacity,
+                'price': self.price,
+                'price_per_ton': self.price_per_ton,
+                'price_per_km': self.price_per_km,
+                'is_price_per_ton': self.is_price_per_ton,
+                'currency': self.currency,
+                'currency_rate': self.currency_rate,
+                'pfactor_vehicle': self.pfactor_vehicle,
+                'pfactor_departure': self.pfactor_departure,
+                'pfactor_arrival': self.pfactor_arrival,
+                'pfactor_distance': self.pfactor_distance,
+                'locale': self.locale.value}
+
+    @classmethod
+    def from_dict(cls, dct):
+        return cls(place_a_name=dct['place_a_name'],
+                   place_a_name_long=dct['place_a_name_long'],
+                   place_b_name=dct['place_b_name'],
+                   place_b_name_long=dct['place_b_name_long'],
+                   map_link=dct['map_link'],
+                   place_chain=dct['place_chain'],
+                   chain_map_link=dct['chain_map_link'],
+                   distance=dct['distance'],
+                   transport_id=dct['transport_id'],
+                   transport_name=dct['transport_name'],
+                   transport_capacity=dct['transport_capacity'],
+                   price=dct['price'],
+                   price_per_ton=dct['price_per_ton'],
+                   price_per_km=dct['price_per_km'],
+                   is_price_per_ton=dct['is_price_per_ton'],
+                   currency=dct['currency'],
+                   currency_rate=dct['currency_rate'],
+                   pfactor_vehicle=dct['pfactor_vehicle'],
+                   pfactor_departure=dct['pfactor_departure'],
+                   pfactor_arrival=dct['pfactor_arrival'],
+                   pfactor_distance=dct['pfactor_distance'],
+                   locale=LocaleDTO(dct['locale']))
---
+++ release/v3.70/settings.py
@@ -0,0 +1,40 @@
+import socket
+
+GOOGLE_APIADR = 'https://maps.googleapis.com/maps/api/distancematrix/json'
+
+GOOGLE_APIKEY_PROD = 'AIzaSyCQKtjuswR2c5fqEJng9k4hDKjWvgDnQLQ'
+GOOGLE_APIKEY_DEV = 'AIzaSyAzEn9je1TvxCJSVIU7UIbv_p7UI8O4rMc'
+
+if socket.gethostname() == 'ltone':
+    GOOGLE_APIKEY = GOOGLE_APIKEY_DEV
+    isDeveloperPC = True
+else:
+    GOOGLE_APIKEY = GOOGLE_APIKEY_PROD
+    isDeveloperPC = False
+
+GOOGLE_MAX_REQUEST_PLACES = 25
+
+DEPOT_DB_LOC = 'dbs/DepotPark_v3.0.sqlite'
+DEPOTPARK_NOSQL_LOC = 'dbs/depotpark_nosql.json'
+STATEPARK_NOSQL_LOC = 'dbs/statepark_nosql.json'
+CACHE_LOCATION = 'dbs/cache.sqlite'
+
+SMS_APIADR = 'https://im.smsclub.mobi/sms/send'
+SMS_APIKEY = 'XXIxH1w1dLNEGmR'
+SMS_ALPHANAME = 'Inter Smart'
+SMS_BLACKLIST = ('380953459607', '380687070075', '380995617113', '380634472972')
+BLACKLIST_FILE_LOC = '../routecalc_blacklist.txt'
+
+TELEGRAM_BOT_APIKEY = '5030273725:AAGOindAi5C4VSvlMsi_hVK2W9IQH13ySwQ'
+TELEGRAM_DIRECT_CHAT_ID = '51147855'
+TELEGRAM_SILENT_CHAT_ID = '-700974331'
+TELEGRAM_LOUD_CHAT_ID = '-649504862'
+TELEGRAM_DEVELOPER_CHAT_ID = '-756003763'
+
+LOG_DB_LOCATION = 'dbs/QueryLog.sqlite'
+
+MAX_REQUESTS = 3
+
+WORKING_DIR = '/home/olivercromwell/Python-Scrapy/RouteCalc/'
+
+SMS_TEXT_REDIAL_PHONE = '+380687070075'
