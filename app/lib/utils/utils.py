3c010a212b26419288dc7b69becb324c4bcc161b
Alex Halitsky <ceasione@gmail.com>
2025-05-26 14:05:31 +0200

    refactor (utils.py): remove redundant code

--- app/lib/utils/utils.py
+++ app/lib/utils/utils.py
@@ -14,14 +14,3 @@ def setup_logger(loglevel: str) -> None:
                         format='%(asctime)s.%(msecs)03d - %(levelname)s - %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
     logging.info(f'loglevel is {loglevel.upper()}')
-
-
-def __sliding_window(iterable, n):
-    # yields a number of lists where len=n sliced from iterable
-    if len(iterable) > n > 0:
-        possible_windows = len(iterable) - n + 1
-        for pointer in range(0, possible_windows):
-            closing = pointer+n
-            yield iterable[pointer:closing]
-    else:
-        yield iterable
