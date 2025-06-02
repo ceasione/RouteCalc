08903b2e6010a6a4be9d93dceb0d778f4da14d07
Alex Halitsky <ceasione@gmail.com>
2025-06-02 19:10:45 +0200

    refactor: convert State to dataclass, add detailed docstrings, and clean up StatePark with type hints and constants

--- app/lib/calc/statepark.py
+++ app/lib/calc/statepark.py
@@ -1,27 +1,26 @@
 import json
 from app import settings
+from pathlib import Path
+from dataclasses import dataclass, asdict
+from typing import List, Optional
+
+DATA_PATH = Path(settings.STATEPARK_NOSQL_LOC)
+CURRENCY_PRIORITY = {'USD': 3, 'EUR': 2, 'UAH': 1}
+CURRENCY_RATES = {'UAH': 1.0, 'USD': 41.0, 'EUR': 45.0}
 
 
 class Currency:
 
-    @classmethod
-    def get_preferred(cls, cur1, cur2):
-        priority = {'USD': 3, 'EUR': 2, 'UAH': 1}
+    iso_code: str
 
-        prio1 = priority[cur1.iso_code]
-        prio2 = priority[cur2.iso_code]
+    @staticmethod
+    def get_preferred(cur1, cur2):
+        prio1 = CURRENCY_PRIORITY[cur1.iso_code]
+        prio2 = CURRENCY_PRIORITY[cur2.iso_code]
+        return cur1 if prio1 >= prio2 else cur2
 
-        if prio1 >= prio2:
-            return cur1
-        else:
-            return cur2
-
-    rates = {'UAH': 1.0,
-             'USD': 41.0,
-             'EUR': 45.0}
-
-    def __init__(self, iso_code):
-        if iso_code not in self.rates.keys():
+    def __init__(self, iso_code: str):
+        if iso_code not in CURRENCY_RATES.keys():
             raise ValueError(f'Currency not set: {iso_code}')
         self.iso_code = iso_code
 
@@ -29,33 +28,54 @@ class Currency:
         return self.iso_code
 
     def rate(self):
-        return self.rates[self.iso_code]
+        return CURRENCY_RATES[self.iso_code]
 
 
-class State:  # As a country as an area not a state of an element or smth
+@dataclass
+class State:
+    """
+    State represents a country and have departure and arrival ratios
+    (which can be reassigned by the Depots although)
+    If Depot doesn't have its ratios - corresponding State's ratios
+    is used instead
 
-    def __init__(self, iso_code: str, currency: str, state_name: str, departure_ratio: float, arrival_ratio: float):
-        self.iso_code = iso_code
-        self.currency = Currency(currency)
-        self.state_name = state_name
-        self.departure_ratio = departure_ratio
-        self.arrival_ratio = arrival_ratio
+    Represents a country (state) with associated currency and transport ratios.
+
+    The State object holds information about a country's ISO code, currency, and
+    its default departure and arrival ratios used to make calculations.
+    These ratios can be overridden by a Depot, if available. If not,
+    the State's ratios are used as fallback values.
+
+    Attributes:
+        iso_code (str): ISO country code.
+        currency (str or Currency): Currency used in the state; converted to a Currency instance on init.
+        state_name (str): Human-readable name of the country/state.
+        departure_ratio (float): Ratio used for calculating departure-related costs.
+        arrival_ratio (float): Ratio used for calculating arrival-related costs.
+
+    Methods:
+        __post_init__(): Ensures currency is an instance of Currency.
+        from_dict(dct): Creates a State instance from a dictionary.
+        to_dict(): Converts the State to a serializable dictionary, stringifying the currency.
+    """
+    iso_code: str
+    currency: str or Currency
+    state_name: str
+    departure_ratio: float
+    arrival_ratio: float
+
+    def __post_init__(self):
+        if isinstance(self.currency, str):
+            self.currency = Currency(self.currency)
 
     @classmethod
     def from_dict(cls, dct):
-        return cls(iso_code=dct['iso_code'],
-                   currency=dct['currency'],
-                   state_name=dct['state_name'],
-                   departure_ratio=dct['departure_ratio'],
-                   arrival_ratio=dct['arrival_ratio'])
+        return cls(**dct)
 
     def to_dict(self):
-        return {
-            'iso_code': self.iso_code,
-            'currency': str(self.currency),
-            'state_name': self.state_name,
-            'departure_ratio': self.departure_ratio,
-            'arrival_ratio': self.arrival_ratio}
+        data = asdict(self)
+        data['currency'] = str(self.currency)
+        return data
 
 
 class StateEncoder(json.JSONEncoder):
@@ -66,39 +86,63 @@ class StateEncoder(json.JSONEncoder):
 
 
 class StatePark:
+    """
+    A container class for managing a collection of `State` instances.
 
-    def __init__(self, already_park: [State] = None):
+    Provides functionality to load states from a JSON file, save them back,
+    convert them to a dictionary format, and retrieve individual states by
+    their ISO code.
+
+    Attributes:
+        park (List[State]): The list of State objects currently in the park.
+
+    Methods:
+        from_file(filename): Class method to create a StatePark from a JSON file.
+        to_file(filename): Saves the current StatePark to a JSON file.
+        as_dict(): Returns a dictionary representation of the StatePark.
+        find_by_iso(iso_code): Returns the State with the given ISO code.
+    """
+    park: List[State]
+
+    def __init__(self, already_park: Optional[List[State]] = None):
         self.park = []
         if already_park is not None and isinstance(already_park, list):
             """This is used by the def from_file(cls, filename) method"""
             self.park = already_park
 
     @classmethod
-    def from_file(cls, filename=settings.STATEPARK_NOSQL_LOC):
-        # Reconstructing from file
+    def from_file(cls, filename: str = DATA_PATH):
         with open(file=filename, mode='r', encoding='utf8', ) as f:
             contents = f.read()
         struct = json.loads(contents)
         park = [State.from_dict(item) for item in struct['statepark']]
         return cls(park)
 
-    def to_file(self, filename=settings.STATEPARK_NOSQL_LOC):
-        # Saving to file
+    def to_file(self, filename: str = settings.STATEPARK_NOSQL_LOC):
         a_dict = self.as_dict()
         a_str = json.dumps(a_dict, ensure_ascii=False)
         with open(file=filename, mode='w') as f:
             f.write(a_str)
 
     def as_dict(self):
-        d = dict()
-        d['statepark'] = [state.to_dict() for state in self.park]
-        return d
+        return {
+            'statepark': [state.to_dict() for state in self.park]
+        }
 
-    def find_by_iso(self, iso_code):
-        for state in self.park:
-            if state.iso_code == iso_code:
-                return state
-        raise RuntimeError(f'No such state: {iso_code}')
+    def find_by_iso(self, iso_code: str) -> State:
+        """
+        Find and return State by iso code
+        :param iso_code: (str) ('UA', 'GB', 'PL') ...
+        :return: State object
+        """
+        try:
+            return next((state for state in self.park if state.iso_code == iso_code))
+        except StopIteration:
+            raise ValueError(f'No such state: {iso_code}')
 
 
-statepark = StatePark.from_file(settings.STATEPARK_NOSQL_LOC)
+if __name__ == '__main__':
+    DATA_PATH = Path('../../../') / settings.STATEPARK_NOSQL_LOC
+
+
+statepark = StatePark.from_file(DATA_PATH)
