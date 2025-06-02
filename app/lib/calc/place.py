0e2b1a8d61f49ad8cdccfd3ce5942690891ab4e1
Alex Halitsky <ceasione@gmail.com>
2025-06-02 19:12:46 +0200

    refactor: add docstrings, improve Place class clarity

--- app/lib/calc/place.py
+++ app/lib/calc/place.py
@@ -9,6 +9,20 @@ APIKEY = settings.GOOGLE_APIKEY
 
 
 class LatLngAble(ABC):
+    """
+    Abstract base class for objects that represent geographic coordinates.
+
+    This class provides equality and hashing behavior for latitude/longitude pairs,
+    rounded to a precision of 6 decimal places for equality and for hashing.
+
+    Attributes:
+        lat (float): Latitude of the location.
+        lng (float): Longitude of the location.
+
+    Methods:
+        __eq__(other): Compares two LatLngAble instances with precision rounding.
+        __hash__(): Returns a hash based on rounded latitude and longitude.
+    """
     def __init__(self, lat: float, lng: float):
         self.lat: float = lat
         self.lng: float = lng
@@ -27,13 +41,24 @@ class LatLngAble(ABC):
 
 
 class Place(LatLngAble):
+    """
+    Represents a geographical place with coordinates and optional metadata.
 
-    def __init__(self,
-                 lat: float,
-                 lng: float,
-                 name=None,
-                 name_long=None,
-                 countrycode=None):
+    Inherits from LatLngAble to provide latitude/longitude-based equality and hashing.
+
+    Attributes:
+        lat (float): Latitude of the place (inherited).
+        lng (float): Longitude of the place (inherited).
+        name (str, optional): Short or common name of the place.
+        name_long (str, optional): Full or extended name of the place.
+        countrycode (str, optional): ISO 3166-1 alpha-2 country code.
+
+    Methods:
+        __repr__(): Returns a string representation of the place.
+        to_dict(): Returns a dictionary representation of the place's attributes.
+    """
+
+    def __init__(self, lat: float, lng: float, name=None, name_long=None, countrycode=None):
         super().__init__(lat, lng)
         self.name = name
         self.name_long = name_long
@@ -43,23 +68,7 @@ class Place(LatLngAble):
         return f'Place [{self.name} lat: {self.lat}, lng: {self.lng}]'
 
     def to_dict(self):
-        d = self.__dict__
-        return d
-
-    @classmethod
-    def from_rqst(cls, rqst):
-        return cls(rqst['lat'],
-                   rqst['lng'],
-                   name=rqst['name_short'],
-                   name_long=rqst['name_long'],
-                   countrycode=rqst['countrycode'])
-
-    @classmethod
-    def from_dict(cls, dct):
-        return cls(lat=dct['lat'],
-                   lng=dct['lng'],
-                   name=dct['name'],
-                   name_long=None)
+        return self.__dict__
 
 
 class PlaceEncoder(json.JSONEncoder):
